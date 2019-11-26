c36420d5cabf8c0fcf92ac3296a26f99578dc584
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-11-26 12:35:15 -0800

    added event listeners and added contact to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,30 @@
 function hideDivs(){
     $(".about-me").show();
     $(".portfolio").hide();
+    $(".contact").hide();
+    $(".resume").hide();
 };
 
+hideDivs();
+
+$(".aboutLink").on("click", function(event){
+    hideDivs();
+})
+
+$(".portfolioLink").on("click", function(event){
+    $(".portfolio").show();
+    $(".about-me").hide();
+    $(".contact").hide();
+})
+
+$(".contactLink").on("click", function(event){
+    $(".portfolio").hide();
+    $(".about-me").hide();
+    $(".contact").show();
+})
+
+$(".resumeLink").on("click", function(event){
+    $(".portfolio").hide();
+    $(".about-me").hide();
+    $(".contact").hide();
+})
