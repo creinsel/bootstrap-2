58c8cc9a7bdd6277ad2c9cf3d45c334b6051db31
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-07-22 10:34:33 -0700

    changing scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,35 @@
-function hideDivs(){
-    $(".about-me").show();
-    $(".portfolio").hide();
-    $(".contact").hide();
-    $(".resume").hide();
-};
+// function hideDivs(){
+//     $(".about-me").show();
+//     $(".portfolio").hide();
+//     $(".contact").hide();
+//     $(".resume").hide();
+// };
 
-hideDivs();
+// hideDivs();
 
-$(".aboutLink").on("click", function(event){
-    hideDivs();
-})
+// $(".aboutLink").on("click", function(event){
+//     hideDivs();
+// })
 
-$(".portfolioLink").on("click", function(event){
-    $(".portfolio").show();
-    $(".about-me").hide();
-    $(".contact").hide();
-    $(".resume").hide();
-})
+// $(".portfolioLink").on("click", function(event){
+//     $(".portfolio").show();
+//     $(".about-me").hide();
+//     $(".contact").hide();
+//     $(".resume").hide();
+// })
 
-$(".contactLink").on("click", function(event){
-    $(".portfolio").hide();
-    $(".about-me").hide();
-    $(".contact").show();
-    $(".resume").hide();
-})
+// $(".contactLink").on("click", function(event){
+//     $(".portfolio").hide();
+//     $(".about-me").hide();
+//     $(".contact").show();
+//     $(".resume").hide();
+// })
 
-$(".resumeLink").on("click", function(event){
-    $(".portfolio").hide();
-    $(".about-me").hide();
-    $(".contact").hide();
-    $(".resume").show();
-})
+// $(".resumeLink").on("click", function(event){
+//     $(".portfolio").hide();
+//     $(".about-me").hide();
+//     $(".contact").hide();
+//     $(".resume").show();
+// })
+
+$('body').scrollspy({ target: '#navbar-example' })
